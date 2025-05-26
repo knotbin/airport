da5246a664604a13230460c294bc5e18a576013e
Roscoe Rubin-Rottenberg <roscoe@knotbin.xyz>
2025-05-26 17:04:28 -0400

    og image

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,9 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="og:image" content="/og-image.jpg" />
+        <meta name="og:title" content="Airport" />
+        <meta name="og:description" content="Airport is an AT Protocol PDS Migration Tool that allows you to seamlessly migrate your account from one PDS to another." />
         <title>Airport</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
