5b9660301f1cdcac5ba4660fffae1f5d56e222bf
Turtlepaw <turtlepaw422@outlook.com>
2025-09-01 18:30:08 +0000

    fix: use localhost

--- tests/utils/config.ts
+++ tests/utils/config.ts
@@ -4,7 +4,7 @@
 
 export const TEST_CONFIG = {
   // Airport application URL
-  airportUrl: process.env.AIRPORT_URL || "http://127.0.0.1:8000",
+  airportUrl: "http://localhost:8000",
   handle: `migration-source.test`,
   targetHandle: `migration-target.test`,
   email: "testuser@example.com",
